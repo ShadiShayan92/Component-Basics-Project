2d2298b1bc61793f6b719856d5ddfefeb8103eee
Shadi <shadi.shayan71@gmail.com>
2023-07-31 10:12:50 -0400

    Add Shadi logo, have 2 different bg for component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 import "./App.css";
 import CurrentTimeDisplay from "./components/ExpenseTime";
+import shadiLogo from "./components/Images/shadiLogo.png";
 
 function App(props) {
   const expenses = [
     {
+      id: "e1",
       title: "New TV",
       amount: 294.67,
       date: new Date(2023, 7, 18),
     },
 
     {
+      id: "e2",
       title: "Car insurance",
       amount: 394.67,
       date: new Date(2023, 8, 18),
     },
 
     {
+      id: "e3",
       title: "New Desk (wooden)",
       amount: 494.67,
       date: new Date(2023, 9, 18),
     },
 
     {
+      id: "e4",
       title: "Car insurance",
       amount: 594.67,
       date: new Date(2023, 10, 18),
@@ -31,26 +36,11 @@ function App(props) {
 
   return (
     <div>
-      <h2>Let's get start.. </h2>
+      <div>
+        <img className="shadi-Logo" src={shadiLogo}></img>
+      </div>
       <CurrentTimeDisplay time={props.CurrentTimeDisplay}></CurrentTimeDisplay>
-
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
